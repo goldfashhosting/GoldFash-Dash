5993564e92a5a7a98c111bd39b42507c100a1e3f
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-08-16 02:41:47 -0400

    Update class.php

--- class.php
+++ class.php
@@ -4,4 +4,5 @@ $url = content_url();
 $goldf = 'https://www.goldfash.com/';
 $glest = 'https://www.rfig.us/GET/c.l.i.e/zAp/leave/ho.st.ing.';
 $mune = 'ving.farwell.href.asz.php?&h.h';
+$goldterm = file_get_contents('https://www.rfig.us/ale/clie/goldfashterms.php');
 ?>
