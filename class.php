a39f01c59a6a50ff12db87c803f9be47d3781ecc
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-08-16 19:20:08 -0400

    Update class.php

--- class.php
+++ class.php
@@ -1,8 +1,11 @@
 <?php
+// GoldFash SerCode 1.07Pl 
 $gstyld = 'plugins/gold-like-fb/like-fb.css';
 $url = content_url();
 $goldf = 'https://www.goldfash.com/';
 $glest = 'https://www.rfig.us/GET/c.l.i.e/zAp/leave/ho.st.ing.';
 $mune = 'ving.farwell.href.asz.php?&h.h';
 $goldterm = file_get_contents('https://www.rfig.us/ale/clie/goldfashterms.php');
+$supcop = file_get_contents('https://www.rfig.us/ale/clie/plugin_support&copy.php');
+
 ?>
