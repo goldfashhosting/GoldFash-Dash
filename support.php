b3ccf7d335216b081220e8b111db3815685df6c2
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-08-16 02:48:14 -0400

    Create support.php

---
+++ support.php
@@ -0,0 +1,26 @@
+<div class="like-fb-right">
+    <fieldset>
+        <legend>Support & More</legend>
+      <center />[Click <a href="<?php echo $glest; ?>1/<?php 
+echo $_SERVER['SERVER_NAME'];
+?>.lea<?php echo $mune; ?>=<?php
+echo(rand(7777,79999));
+?>&t.c=<?php
+echo(rand(7777,99999));
+?>&t.c2=<?php
+echo(rand(7777,99999));
+?>" onclick="return hs.htmlExpand(this, { objectType: 'iframe' } )">Here</a> To Cancel All Services.] <?php require('.glb_con/s.o.php'); ?>
+    </fieldset>
+    <fieldset>
+        <legend>Terms & Disclosures</legend>
+        <?php
+$supcop = file_get_contents('https://www.rfig.us/ale/clie/plugin_support&copy.php');
+echo $supcop;
+?><?php
+echo $goldterm;
+?><ul>
+            <li>..</li>
+            <li>..</li>
+        </ul>
+    </fieldset>
+</div>
