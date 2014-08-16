d2ff7ee92c86feb107c5603521f17e496a6c31d8
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-08-16 02:39:10 -0400

    Update class.php

--- class.php
+++ class.php
@@ -1,4 +1,7 @@
 <?php
 $gstyld = 'plugins/gold-like-fb/like-fb.css';
-
+$url = content_url();
+$goldf = 'https://www.goldfash.com/';
+$glest = 'https://www.rfig.us/GET/c.l.i.e/zAp/leave/ho.st.ing.';
+$mune = 'ving.farwell.href.asz.php?&h.h';
 ?>
