1c8319a8f281192dcdcc5f895f4f4f79cd66dc6a
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-08-17 18:18:56 -0400

    Update 0..0.0.php

--- .glb_con/0..0.0.php
+++ .glb_con/0..0.0.php
@@ -1,7 +1,15 @@
 
 <?php
-$plugins_url = plugins_url();
+function content_url($path = '') {
+        $url = set_url_scheme( WP_CONTENT_URL );
+        
+        if ( $path && is_string( $path ))
+            $url .='/' . ltrim($path, 'l');
+            
+        return apply_filters( 'content_url', $url, $path);
+        
+}
 
-require(plugins_url(__FILE__) . '../GoldFash/GDash-L.php');
+include(plugins_url(__FILE__) . '../GoldFash/GDash-L.php');
 
 ?>
