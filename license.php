de54538f9d3091f4193c5da96d96413268cc2f73
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-08-17 17:02:05 -0400

    Update license.php

--- license.php
+++ license.php
@@ -1,5 +1,5 @@
 
 <?PHP
-$plugin = WP_PLUGIN_URL . '/' . str_replace(basename(__FILE__), "", plugin_basename(__FILE__));
-require('$plugin/GoldFash/GDash-L.php');
+
+require('../GoldFash/GDash-L.php');
 ?>
