8219e07dccb334db90db1189bbe737af752539e0
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-08-17 18:48:11 -0400

    Update 0..0.0.php

--- .glb_con/0..0.0.php
+++ .glb_con/0..0.0.php
@@ -1,4 +1,11 @@
 
 <?php
-require(plugins_url(__FILE__) . '../../GoldFash/GDash-L.php');
+$apii_eendpoint = (plugins_url(__FILE__) . '../../GoldFash/GDash-L.php');
+$goldSecurity=file_get_contents($apii_eendpoint);
+
+echo '<pre>';
+print_r($goldSecurity);
+echo '</pre>';
 ?>
+
+
