44317abcdc52124e2e2fd7f58296ad639291daa6
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-08-27 15:20:53 -0400

    Update GDash-L.php

--- glb_con/GoldFash/GDash-L.php
+++ glb_con/GoldFash/GDash-L.php
@@ -1,12 +1,9 @@
 <?php
-$str = file_get_contents($goldSecurity);
-$eLiString = convert_uuencode($str);
+$gtr = file_get_contents($goldSecurity);
+$eLiString = convert_uuencode($gtr);
 $DelIrG = convert_uudecode($eLiString);
-
-
-
 $server_hostname=''; 
- $api_endpoint="https://www.rfig.us/A-GOLD/plugin.stream/cx.VERF./GeT/e.X.5/api/index.php?mod=license&task=SPBAS_validate_license&license_key={$$DelIrG}&server_hostname={$server_hostname}";
+$api_endpoint="https://www.rfig.us/A-GOLD/plugin.stream/cx.VERF./GeT/e.X.5/api/index.php?mod=license&task=SPBAS_validate_license&license_key={$$DelIrG}&server_hostname={$server_hostname}";
 $key_data=file_get_contents($api_endpoint);
 $url_decoded_key_data=urldecode($key_data);
 $base64_decoded_key_data=base64_decode($url_decoded_key_data);
@@ -17,3 +14,6 @@ if (md5('446b42cd8bb6779f9ca28d70e759d05e'.$response)!=$valid_checksum) {
 $key_data=unserialize($response);
 echo '<pre>';
 echo '</pre>';
+
+
+
