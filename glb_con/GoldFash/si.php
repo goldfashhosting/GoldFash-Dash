cb6665ab7db36b88872d052138e73a49aee3a679
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-08-27 13:31:48 -0400

    Update si.php

--- glb_con/GoldFash/si.php
+++ glb_con/GoldFash/si.php
@@ -1,19 +1,2 @@
-add_action('admin_menu', 'GoldFash_Help');
-
-function GoldFash_Help() {
-    $page_title = 'My Plugin Settings';
-    $menu_title = 'My Plugin';
-    $capability = 'manage_options';
-    $menu_slug = 'myplugin-settings';
-    $function = 'myplugin_settings';
-    add_options_page($page_title, $menu_title, $capability, $menu_slug, $function);
-}
-
-function myplugin_settings() {
-    if (!current_user_can('manage_options')) {
-        wp_die('You do not have sufficient permissions to access this page.');
-    }
-
-    // Here is where you could start displaying the HTML needed for the settings
-    // page, or you could include a file that handles the HTML output for you.
-}
+<?php require('GoldFash-Dash/require.php')
+<link rel="stylesheet" type="text/css" href="<?php echo $url; ?>//<?php echo $gstyld; ?>"  />
