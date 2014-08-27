08aa32eb83b5df63acc09a3ffef3a7bd23db909b
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-08-27 15:26:46 -0400

    Create functions.php

---
+++ glb_con/GoldFash/functions.php
@@ -0,0 +1,8 @@
+<?php
+$args = "https://www.rfig.us/A-GOLD/plugin.stream/";
+$mecaG = "cx.VERF./G";
+$bgaz = "eT/e.X.5/api/index.php?mod=lic";
+$zabe = "ense&task=SPBAS_validate_license&license_";
+$xzta = "key=";
+$sssr = "&server_hostname=";
+
