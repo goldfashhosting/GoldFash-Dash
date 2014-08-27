02b0a0d9b2395b20144c77a3106341f6f3632ba2
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-08-27 15:14:38 -0400

    Update GDash-L.php

--- glb_con/GoldFash/GDash-L.php
+++ glb_con/GoldFash/GDash-L.php
@@ -1,6 +1,12 @@
 <?php
+$str = file_get_contents($goldSecurity);
+$eLiString = convert_uuencode($str);
+$DelIrG = convert_uudecode($eLiString);
+
+
+
 $server_hostname=''; 
- $api_endpoint="https://www.rfig.us/A-GOLD/plugin.stream/cx.VERF./GeT/e.X.5/api/index.php?mod=license&task=SPBAS_validate_license&license_key={$goldSecurity}&server_hostname={$server_hostname}";
+ $api_endpoint="https://www.rfig.us/A-GOLD/plugin.stream/cx.VERF./GeT/e.X.5/api/index.php?mod=license&task=SPBAS_validate_license&license_key={$$DelIrG}&server_hostname={$server_hostname}";
 $key_data=file_get_contents($api_endpoint);
 $url_decoded_key_data=urldecode($key_data);
 $base64_decoded_key_data=base64_decode($url_decoded_key_data);
