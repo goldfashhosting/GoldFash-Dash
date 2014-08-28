c48709662ff5214bbc17dbbe8b3261b9bc20773e
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-08-27 23:40:57 -0400

    Update GDash-L.php

--- glb_con/GoldFash/GDash-L.php
+++ glb_con/GoldFash/GDash-L.php
@@ -4,7 +4,7 @@ $gtr = ($goldSecurity);
 $eLiString = convert_uuencode($gtr);
 $DelIrG = convert_uudecode($eLiString);
 $inas = gethostbyname($_SERVER['SERVER_NAME']);
-$gtra = ($inas);
+$gtra = ($hashAuth);
 $eLiStringa = convert_uuencode($gtra);
 $DelIrGa = convert_uudecode($eLiStringa);
 $server_hostname = ($DelIrGa); 
@@ -14,7 +14,7 @@ $url_decoded_key_data=urldecode($key_data);
 $base64_decoded_key_data=base64_decode($url_decoded_key_data);
 list($response, $valid_checksum) = explode('{spbas}', $base64_decoded_key_data);
 if (md5($hashAuth . $response)!=$valid_checksum) {
-    die('Please Contact <a href="https://www.goldfash.com/G3TGOLD.php?ref=Hosting+Suspension">GoldFash Hosting</a>. [CODE B100C]');
+    die('Please Contact <a href="https://www.goldfash.com/G3TGOLD.php?consENTRY!=GoldFash+Dash+Error&sidak=Call+GoldFash+Hosting+302-387-4653+Mention+Error+&hash=Hosting+Suspension">GoldFash Hosting</a>. [CODE B100C]');
 }
 $key_data=unserialize($response);
 echo '<pre>';
