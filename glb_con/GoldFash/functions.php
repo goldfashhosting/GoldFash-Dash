4e8c8e9e3b1795dd9adda1a991d4a9a1420ba6fb
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-08-28 09:18:14 -0400

    Update functions.php

--- glb_con/GoldFash/functions.php
+++ glb_con/GoldFash/functions.php
@@ -6,11 +6,9 @@ $zabe = "ense&task=SPBAS_validate_license&license_";
 $xzta = "key=";
 $sssr = "&server_hostname=";
 $gstyld = 'plugins/gold-like-fb/like-fb.css';
-$url = content_url();
 $goldf = 'https://www.goldfash.com/';
 $glest = 'https://www.rfig.us/GET/c.l.i.e/zAp/leave/ho.st.ing.';
 $mune = 'ving.farwell.href.asz.php?&h.h';
 $goldterm = file_get_contents('https://www.rfig.us/ale/clie/goldfashterms.php');
 $supcop = file_get_contents('https://www.rfig.us/ale/clie/plugin_support&copy.php');
-$plugin = WP_PLUGIN_URL . '/' . str_replace(basename(__FILE__), "", plugin_basename(__FILE__));
 
