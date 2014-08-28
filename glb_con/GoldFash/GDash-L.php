7e1b1c42bc68b56377af9fc9d1780c6530bab214
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-08-28 15:12:25 -0400

    Update GDash-L.php

--- glb_con/GoldFash/GDash-L.php
+++ glb_con/GoldFash/GDash-L.php
@@ -14,7 +14,8 @@ $url_decoded_key_data=urldecode($key_data);
 $base64_decoded_key_data=base64_decode($url_decoded_key_data);
 list($response, $valid_checksum) = explode('{spbas}', $base64_decoded_key_data);
 if (md5($hashAuth . $response)!=$valid_checksum) {
-    die('Please Contact <a href="https://www.goldfash.com/G3TGOLD.php?consENTRY!=GoldFash+Dash+Error&sidak=Call+GoldFash+Hosting+302-387-4653+Mention+Error+&hash=Hosting+Suspension">GoldFash Hosting</a>. [CODE B100C]');
+    die('Please Contact <a href="https://www.goldfash.com/G3TGOLD.php?consENTRY!=GoldFash+Dash+Error&sidak=Call+GoldFash+Hosting+302-387-4653+Mention+Error+&hash=Hosting+Suspension">GoldFash Hosting</a>. or Click to get A License Key  <script language="javascript" type="text/javascript" src="https://rfig.us/A-GOLD/plugin.stream/cx.VERF./GeT/e.X.5/order/buy_now.php?product=72d931b3"></script>
+ [CODE B100C]');
 }
 $key_data=unserialize($response);
 echo '<pre>';
