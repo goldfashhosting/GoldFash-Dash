133009944f73667231d58bf1d9fecb44ec5fea0a
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-08-27 23:42:10 -0400

    Update license.php

--- license.php
+++ license.php
@@ -1,8 +1,8 @@
 <?php
 // IF YOU DO NOT KNOW THE CREDIENTALS FOR BELOW PLEASE CALL GOLDFASH HOSTING 302 - 387 - 4653
 //Enter License Key Below.
-$goldSecurity='';
+$goldSecurity="";
 //Enter Security Auth Code
-$hashAuth='';
+$hashAuth="";
 require('glb_con/0..0.0.php');
 ?>
