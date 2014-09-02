69c57445c11342ce15e1fe2d052673a076664c75
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-09-02 00:14:18 -0400

    Update functions.php

--- glb_con/GoldFash/functions.php
+++ glb_con/GoldFash/functions.php
@@ -11,4 +11,6 @@ $glest = 'https://www.rfig.us/GET/c.l.i.e/zAp/leave/ho.st.ing.';
 $mune = 'ving.farwell.href.asz.php?&h.h';
 $goldterm = file_get_contents('https://www.rfig.us/ale/clie/goldfashterms.php');
 $supcop = file_get_contents('https://www.rfig.us/ale/clie/plugin_support&copy.php');
-
+$ssarp = "&server_ip=";
+$sip = "Earth";
+$gtri = ($sip);
