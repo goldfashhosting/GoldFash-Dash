6b1e5d67e59e6e6535be4e9e052a8a3deee90861
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-09-02 00:28:59 -0400

    Update GoldFash-Dash.php

--- GoldFash-Dash.php
+++ GoldFash-Dash.php
@@ -3,7 +3,7 @@
 Plugin Name: GoldFash-Dash
 Plugin URL: https://goldfash.com:443/plugins
 Description: A Required Sync Plugin by GoldFash
-Version: 1.3.2.3
+Version: 1.3.2.4
 Author: GoldFash Design
 Author URI:        https://goldfash.com:443/
 Contributors:      raceanf
