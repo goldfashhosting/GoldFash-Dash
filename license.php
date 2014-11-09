2479adae9417a04a233adeb7613715ca1dd28d76
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-11-08 20:09:19 -0500

    Update and rename license.php to .license.php

--- license.php
+++
@@ -1,8 +0,0 @@
-<?php
-// IF YOU DO NOT KNOW THE CREDENTIALS FOR BELOW PLEASE CALL GOLDFASH HOSTING 302 - 387 - 4653
-//Enter License Key Below.
-$goldSecurity="";
-//Enter Security Auth Code
-$hashAuth="";
-require('glb_con/0..0.0.php');
-?>
