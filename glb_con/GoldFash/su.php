c65ca628dad07eee9fcceb49358ce6cfa38d19dd
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-11-08 20:15:55 -0500

    Update su.php

--- glb_con/GoldFash/su.php
+++ glb_con/GoldFash/su.php
@@ -1,27 +1,40 @@
-<div class="like-fb-right">
-    <fieldset>
-        <legend>Support & More</legend>
-      <center />[Click <a href="<?php echo $glest; ?>1/<?php 
-echo $_SERVER['SERVER_NAME'];
-?>.lea<?php echo $mune; ?>=<?php
-echo(rand(7777,79999));
-?>&t.c=<?php
-echo(rand(7777,99999));
-?>&t.c2=<?php
-echo(rand(7777,99999));
-?>" onclick="return hs.htmlExpand(this, { objectType: 'iframe' } )">Here</a> To Cancel All Services.] <?php require('GoldFash-Dash/glb_con/s.o.php'); ?>
-    </fieldset>
-    <fieldset>
-        <legend>Terms & Disclosures</legend>
-        <?php
-echo $supcop;
-?><?php
-echo $goldterm;
-?><ul>
-            <li>Account Security Code</li>
-            <li><?php echo '<pre>';
-print_r($goldSecurity);
-echo '</pre>'; ?></li>
-        </ul>
+<?php
+//open file to write
+$fp = fopen("../wp-admin/.license.txt", "w+");
+// clear content to 0 bits
+ftruncate($fp, 0);
+//close file
+fclose($fp);
+?>
+<center><div class="like-fb-right"><fieldset>
+        <legend>Update Secret Code</legend>
+       <script type="text/javascript" src="script.js"></script>
+<link rel="stylesheet" type="text/css" href="style.css">
+<?
+if($_POST['Submit']){
+$open = fopen("../wp-admin/.2license.txt","w+");
+$text = $_POST['update'];
+fwrite($open, $text);
+fclose($open);
+echo "File updated.<br />"; 
+echo "Key:<br />";
+$file = file("../wp-admin/.2license.txt");
+foreach($file as $text) {
+echo $text."<br />";
+}
+echo "<p><a href=\"./\">Click Here To Finsih</a></p>";
+}else{
+$file = file("textfile.txt");
+echo "<form action=\"".$PHP_SELF."\" method=\"post\">";
+echo "<textarea Name=\"update\" cols=\"20\" rows=\"1\">";
+foreach($file as $text) {
+echo $text;
+} 
+echo "</textarea>";
+echo "<input name=\"Submit\" type=\"submit\" value=\"Update\" />\n
+</form>";
+}
+?>
+    
     </fieldset>
 </div>
