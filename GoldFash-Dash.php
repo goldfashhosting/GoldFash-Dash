86696d9e9f4b5dc3b69f9ec77b24289d5b851048
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-11-08 20:30:00 -0500

    Update GoldFash-Dash.php

--- GoldFash-Dash.php
+++ GoldFash-Dash.php
@@ -3,7 +3,7 @@
 Plugin Name: GoldFash-Dash
 Plugin URL: https://goldfash.com:443/plugins
 Description: GoldFash.com Hosting Tools Required To Access Server
-Version: 2.0
+Version: 2.0.1
 Author: GoldFash Design
 Author URI:        https://goldfash.com:443/
 Contributors:      raceanf
