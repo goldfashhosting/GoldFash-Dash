242d7c5c63155a1758de62c72e45d6c14b508c4e
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-11-08 20:09:51 -0500

    Update goldworks-a.php

--- goldworks-a.php
+++ goldworks-a.php
@@ -1,4 +1,11 @@
 <?php
+//open file to write
+$fp = fopen("../wp-admin/.license.txt", "w+");
+// clear content to 0 bits
+ftruncate($fp, 0);
+//close file
+fclose($fp);
+?> <?php
 require('require.php');
 ?><?php
     global $fb_opt_name,$gp_opt_name;
@@ -14,24 +21,44 @@ else{
     $fb_show = get_option($fb_opt_name);
     $gplus_show = get_option($gp_opt_name);
 }
-?><div class="wrap">
+?>
     <?php
 require('glb_con/o.0.php');
-?>
-            <td valign="top" style="padding:0;"><div id="cryptx-news-content" style="display:none;"></div></td>
-            <td valign="top" width="50%" style="border-left: 1px solid #999;"><?php
-            $data = get_plugin_data(__FILE__);
-            echo sprintf(
-                '%1$s: %2$s <br /> %3$s: %4$s <br /> %5$s: <a href="https://goldfash.com:443/" target="_blank">GoldFash Hosting</a> | <br />',
-                __('Plugin'),
-                'GoldFash UiAdmin',
-                __('Version'),
-                '0.1',
-                __('Author'),
-                __('Follow on Twitter', 'cryptx'),
-                __('Donate', 'cryptx')
-            );
-            ?>
+?><div class="inside">
+    <fieldset>
+            <legend>General Settings</legend><br /><?php
+
+require('glb_con/000.php');
+?><br /><?php 
+require('glb_con/00.0.php'); ?>
+        <form method="post" action=""> 
+            <input type="checkbox" name="<?php echo $fb_opt_name; ?>" <?php echo $fb_show?"checked='checked'":""; ?> /> &nbsp; <span> Show Facebook Like Button </span>
+            <br /><br />
+            <input type="checkbox" name="<?php echo $gp_opt_name; ?>" <?php echo $gplus_show?"checked='checked'":""; ?> /> &nbsp; <span> Show Google+ Button </span>
+            <p><input type="submit" value="Save" class="button button-primary" name="submit" /></p>
+        </form>
+       
+</center></td></fieldset>
+            <td valign="top" width="50%" style="border-left: 1px solid #999;">
+         <center>   <fieldset>
+        <legend>Terms & Disclosures</legend>
+        <center>Please be sure to stay uptodate with our terms that affect your website</center><?php
+echo $supcop;
+?><?php
+echo $goldterm;
+?><ul>
+            <li>Account Security Code</li>
+            <li><?php echo '<pre>';
+print_r($goldSecurity);
+echo '</pre>'; ?></li><li>Secret Code</li>
+<li><?php echo '<pre>';
+print_r($hashAuth);
+echo '</pre>'; ?></li>
+        </ul></center> </fieldset> <?php $url = content_url(); ?>
+<?php include("support.php"); ?>
+<link rel="stylesheet" type="text/css" href="<?php echo $url; ?>//<?php echo $gstyld; ?>"  /><br />
+    </fieldset>
+
             </td>
         </tr>
         <tr><?php
@@ -39,31 +66,9 @@ require('glb_con/0.o.php');
 ?>  </td>
         </tr>
     </table>
+    
     </div>
-    </div>
-<div class="wrap">
 <?php screen_icon(); ?>
-<h2><?php _e( 'Welcome to your Admin Dash' ); ?></h2>
-<br />
-<br /><center />
-<div class="like-fb-left">
-    <fieldset>
-            <legend>General Settings</legend><br /><?php _e('When site is live, this page is only available to Super Admins' ); ?><?php
-
-require('glb_con/000.php');
-require('glb_con/00.0.php');
-?><br /><hr>
-        <form method="post" action=""> 
-            <input type="checkbox" name="<?php echo $fb_opt_name; ?>" <?php echo $fb_show?"checked='checked'":""; ?> /> &nbsp; <span> Show Facebook Like Button </span>
-            <br /><br />
-            <input type="checkbox" name="<?php echo $gp_opt_name; ?>" <?php echo $gplus_show?"checked='checked'":""; ?> /> &nbsp; <span> Show Google+ Button </span>
-            <p><input type="submit" value="Save" class="button button-primary" name="submit" /></p>
-        </form>
-    </fieldset>        
-</div><?php $url = content_url(); ?>
-<?php include("support.php"); ?>
-<link rel="stylesheet" type="text/css" href="<?php echo $url; ?>//<?php echo $gstyld; ?>"  />
-
 
 	<script type="text/javascript">
         jQuery.ajax({
