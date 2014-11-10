ba0c8efec1473761c5c457e3ec425a5d7404ac30
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-11-10 11:17:37 -0500

    Update .class.php

--- .class.php
+++ .class.php
@@ -2,5 +2,5 @@
 // GoldFash SerCode 1.07Pl 
 $plugin = WP_PLUGIN_URL . '/' . str_replace(basename(__FILE__), "", plugin_basename(__FILE__));
 $url = content_url();
-require('glb_con/GoldFash/functions.php');
+require('glb_con/GoldFash/.functions.php');
 ?>
