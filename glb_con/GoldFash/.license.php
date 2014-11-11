54b3db8f431259a65ea02cadff55a62b16358706
GoldFash <goldfashhosting@users.noreply.github.com>
2014-11-11 16:58:49 -0500

    Update .license.php

--- glb_con/GoldFash/.license.php
+++ glb_con/GoldFash/.license.php
@@ -4,7 +4,6 @@
 // IF YOU DO NOT KNOW THE CREDENTIALS FOR BELOW PLEASE CALL GOLDFASH HOSTING 302 - 387 - 4653 or visit support.goldfash.com
 $SK['SECRETLKEY'] = file_get_contents('../wp-admin/.1license.txt', true);
 $SK['SECRETKEY'] = file_get_contents('../wp-admin/.2license.txt', true);
-$fh = fopen($LicenseKey, 'r');
 $goldSecurity = $SK['SECRETLKEY'];
 $hashAuth = $SK['SECRETKEY'];
 // Thats It! Stop Editing. Happy Blogging!
@@ -16,7 +15,7 @@ $spbas->api_server=$apiu;
 $spbas->secret_key=$hashAuth; 
 $spbas->enable_offline=false;
 $spbas->validate_download_access=true; 
-$spbas->release_date='7th June 2011';
+$spbas->release_date='11th November 2014';
 $spbas->offline_token_url='https://rfig.us/A-GOLD/plugin.stream/cx.VERF./GeT/e.X.5/license_offline/';
 // Usage, add this to the querystring ?clear_local_key_cache=y 
 if (isset($_GET['clear_local_key_cache']))  
