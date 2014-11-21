696d945bc46b9b492781dcb77f0011df67d7c573
GoldFash <goldfashhosting@users.noreply.github.com>
2014-11-21 16:02:05 -0500

    Update .si.php

--- glb_con/GoldFash/.si.php
+++ glb_con/GoldFash/.si.php
@@ -1,68 +1,26 @@
 <?php
-//open file to write
 $fp = fopen("../wp-admin/.license.txt", "w+");
-// clear content to 0 bits
 ftruncate($fp, 0);
-//close file
+fclose($fp); ?><?php $fp = fopen("../wp-admin/.ltextfile.txt", "w+");
+ftruncate($fp, 0);
+fclose($fp); ?><?php $fp = fopen("../wp-admin/.textfile.txt", "w+");
+ftruncate($fp, 0);
 fclose($fp);
 ?>
-<?php
-//open file to write
-$fp = fopen("../wp-admin/.ltextfile.txt", "w+");
-// clear content to 0 bits
-ftruncate($fp, 0);
-//close file
-fclose($fp);
-?>
-<?php
-//open file to write
-$fp = fopen("../wp-admin/.textfile.txt", "w+");
-// clear content to 0 bits
-ftruncate($fp, 0);
-//close file
-fclose($fp);
-?><?php
-$args = "https://www.rfig.us/A-GOLD/plugin.stream/";
-$mecaG = "cx.VERF./G";
-$bgaz = "eT/e.X.5/api/index.php?mod=lic";
-$zabe = "ense&task=SPBAS_validate_license&license_";
-$xzta = "key=";
-$sssr = "&server_hostname=";
-$gstyld = 'plugins/gold-like-fb/like-fb.css';
-$goldf = 'https://www.goldfash.com/';
-$glest = 'https://www.rfig.us/GET/c.l.i.e/zAp/aGreement/';
-$mune = 'ving.farwell.href.asz.php?&h.h';
-$goldterm = file_get_contents('https://www.rfig.us/ale/clie/goldfashterms.php');
-$supcop = file_get_contents('https://www.rfig.us/ale/clie/plugin_support&copy.php');
-$validate = file_get_contents('https://www.rfig.us/A-GOLD/plugin.stream/.assetvalid.php');
-$ssarp = "&server_ip=";
-$sip = "USAsi";
-$gtri = ($sip);
-$url = content_url();
-?>
-<script type="text/javascript" src="https://www.rfig.us:443/A-GOLD/highslide/highslide-with-html.js"></script>
-<link rel="stylesheet" type="text/css" href="https://www.rfig.us:443/A-GOLD/highslide/highslide.css" />
+<?php require('.require.php'); ?>
 <script type="text/javascript">
 hs.graphicsDir = 'https://www.rfig.us:443/A-GOLD/highslide/graphics/';
 hs.outlineType = 'rounded-white';
 hs.wrapperClassName = 'draggable-header';
-
 </script>
 <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>//<?php echo $gstyld; ?>"  />
 <center /><br /><hr>
-<div class="like-fb-left">
+<?php include $div[one]; ?>
     <fieldset>
         <legend>Sponsorship Support & More</legend>
-      <center />[Click <a href="<?php echo $glest; ?>v1.0/<?php 
-echo $_SERVER['SERVER_NAME'];
-?>.lea<?php echo $mune; ?>/=<?php
-echo(rand(7777,79999));
-?>&t.c/=<?php
-echo(rand(7777,99999));
-?>&t.c2/=<?php
-echo(rand(7777,99999));
-?>" onclick="return hs.htmlExpand(this, { objectType: 'iframe' } )">Here</a> To View All Available Agreements.] 
-    </div></fieldset><center><div class="like-fb-center"><fieldset>
+      <center />[Click <a href="<?php echo $glest; ?>v1.0/<?php echo $_SERVER['SERVER_NAME']; ?>.lea
+      <?php echo $mune; ?>/=<?php echo $heiz; ?>&t.c/=<?php echo $heiz; ?>&t.c2/=<?php echo $heiz; ?>" onclick="return hs.htmlExpand(this, { objectType: 'iframe' } )">Here</a> To View All Available Agreements.] 
+    </div></fieldset><center><?php include $div[one]; ?><fieldset>
         <legend>Update License Key</legend>
 <?
 if($_POST['Submit']){
@@ -76,7 +34,7 @@ $file = file("../wp-admin/.1license.txt");
 foreach($file as $text) {
 echo $text."<br />";
 }
-echo "<p><a href=\"?page=goldgb-help\">Click Here For Step 2</a></p>";
+echo "<p><a href=\"../wp-admin/index.php?page=goldgb-help\">Click Here For Step 2</a></p>";
 }else{
 $file = file("../wp-admin/.textfile.txt");
 echo "<form action=\"".$PHP_SELF."\" method=\"post\">";
@@ -92,7 +50,7 @@ echo "<input name=\"Submit\" type=\"submit\" value=\"Update\" />\n
 
     
     </fieldset>
-    <center><div class="like-fb-right"><fieldset>
+    <center><?php include $div[one]; ?><fieldset>
         <legend>Info:</legend>
 <center> Only License Keys from GoldFash Will Work. <strong>You Must Complete Both Steps.</strong>
     <?php echo $validate; ?>
