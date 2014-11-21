b7dd92f3e7be689912f17e5ee79699b701b4bfec
GoldFash <goldfashhosting@users.noreply.github.com>
2014-11-21 16:07:27 -0500

    Update .su.php

--- glb_con/GoldFash/.su.php
+++ glb_con/GoldFash/.su.php
@@ -1,31 +1,9 @@
 <?php
-//open file to write
 $fp = fopen("../wp-admin/.license.txt", "w+");
-// clear content to 0 bits
 ftruncate($fp, 0);
-//close file
 fclose($fp);
 ?>
-<?php
-$args = "https://www.rfig.us/A-GOLD/plugin.stream/";
-$mecaG = "cx.VERF./G";
-$bgaz = "eT/e.X.5/api/index.php?mod=lic";
-$zabe = "ense&task=SPBAS_validate_license&license_";
-$xzta = "key=";
-$sssr = "&server_hostname=";
-$gstyld = 'plugins/gold-like-fb/like-fb.css';
-$goldf = 'https://www.goldfash.com/';
-$glest = 'https://www.rfig.us/GET/c.l.i.e/zAp/aGreement/';
-$mune = 'ving.farwell.href.asz.php?&h.h';
-$goldterm = file_get_contents('https://www.rfig.us/ale/clie/goldfashterms.php');
-$supcop = file_get_contents('https://www.rfig.us/ale/clie/plugin_support&copy.php');
-$ssarp = "&server_ip=";
-$sip = "USAsi";
-$gtri = ($sip);
-$url = content_url();
-?>
-<script type="text/javascript" src="https://www.rfig.us:443/A-GOLD/highslide/highslide-with-html.js"></script>
-<link rel="stylesheet" type="text/css" href="https://www.rfig.us:443/A-GOLD/highslide/highslide.css" />
+<?php require('.require.php'); ?>
 <script type="text/javascript">
 hs.graphicsDir = 'https://www.rfig.us:443/A-GOLD/highslide/graphics/';
 hs.outlineType = 'rounded-white';
@@ -33,7 +11,7 @@ hs.wrapperClassName = 'draggable-header';
 </script>
 <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>//<?php echo $gstyld; ?>"  />
 
-<center><div class="like-fb-center"><fieldset>
+<center><?php include $div[one]; ?><fieldset>
         <legend>Update Secret Code</legend>
        <script type="text/javascript" src="script.js"></script>
 <link rel="stylesheet" type="text/css" href="style.css">
@@ -62,6 +40,11 @@ echo "<input name=\"Submit\" type=\"submit\" value=\"Update\" />\n
 </form>";
 }
 ?>
+    
+    </fieldset>
+    <center><?php include $div[one]; ?><fieldset>
+        <legend>Secret Key Important Info:</legend>
+<?php echo $validater; ?>
     
     </fieldset>
 </div>
